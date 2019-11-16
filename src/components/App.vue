e63723564e2e16d9a99cdc9abe5be7c022c5faca
eduardo <eduardo@logcomex.com>
2019-11-16 19:05:33 -0300

    - Updating main component name to App - Added header w/ searchbar and menu

---
+++ src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="header">
+      <img src="../images/logo-pink.png" alt="logo">
+      <input class="searchbar" type="text" placeholder="Search Companies">
+      <span>OR</span>
+      <button>
+        NEW SMARTLIST
+      </button>
+      <ul class="menu">
+        <li>
+          PROSPECT
+        </li>
+        <li>
+          ALERT
+        </li>
+        <li>
+          LEARN
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <div>
+        <ul class="breadcrumb">
+          <li><a href="#">Home</a></li>
+        </ul>
+        <h1>Hello world!</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
