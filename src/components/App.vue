c27150457fb842771e113f8cd15b21ac164a7344
eduardo <eduardo@logcomex.com>
2019-11-17 20:25:05 -0300

    content html, footer html and css

--- src/components/App.vue
+++ src/components/App.vue
@@ -19,13 +19,46 @@
         </li>
       </ul>
     </div>
-    <div class="content">
-      <div>
-        <ul class="breadcrumb">
-          <li><a href="#">Home</a></li>
-        </ul>
-        <h1>Hello world!</h1>
+    <div class="wrapper">
+      <div class="content">
+        <div class="side">
+          <ul class="breadcrumb">
+            <li><a href="#">home</a></li>
+            <li><a href="#">company page</a></li>
+          </ul>
+          <div style="border-radius: 5px">
+            <ul class="side-menu">
+              <li><a href="#">company data</a></li>
+              <li><a href="#">company table</a></li>
+              <li class="selected"><a href="#">company page</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="main">
+
+        </div>
       </div>
+    </div>
+    <div class="footer">
+      <ul class="bottom-menu left">
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Webinars</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Help Center</a></li>
+      </ul>
+      <ul class="bottom-menu float-right">
+        <li><a href="#">Terms and Conditions</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+      </ul>
+      <p class="copyright">
+        © Copyright 2017, Intrincately, Inc. All Rights Reserved.
+      </p>
+      <ul class="bottom-menu icons float-right">
+        <li><a href="#"><img src="../images/fill-118.png"></a></li>
+        <li><a href="#"><img src="../images/fill-92.png"></a></li>
+        <li><a href="#"><img src="../images/fill-93.png"></a></li>
+      </ul>
     </div>
   </div>
 </template>
