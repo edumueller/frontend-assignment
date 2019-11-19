6ae3557d482a63f0d8a0437f227dae1382b24fda
eduardo <eduardo@logcomex.com>
2019-11-19 18:55:26 -0300

    - Added company activity section scaffolding and partial data - Adjusted some css and added new classes to make things work correctly

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="header">
-      <img src="../images/logo-pink.png" alt="logo">
+      <img class="logo" src="../images/logo-pink.png" alt="logo">
       <input class="searchbar" type="text" placeholder="Search Companies">
       <span>or</span>
       <button>
@@ -18,28 +18,62 @@
           learn
         </li>
       </ul>
+      <ul class="icons float-right">
+        <li><a href="#"><img class="iconize" src="../images/chrome-copy-7.png"></a></li>
+        <li><a href="#"><img class="iconize" src="../images/group.png"></a></li>
+        <li><a href="#"><img src="../images/arrow-down.png"></a></li>
+      </ul>
     </div>
-    <div class="wrapper">
-      <div class="content">
-        <div class="side">
-          <ul class="breadcrumb">
-            <li><a href="#">home</a></li>
-            <li><a href="#">company page</a></li>
+    <div class="content wrapper float-left">
+      <ul class="breadcrumb">
+        <li><a href="#">home</a></li>
+        <li><a href="#">company page</a></li>
+      </ul>
+      <div class="side float-left">
+        <div style="border-radius: 5px">
+          <ul class="side-menu">
+            <li><a href="#">company data</a></li>
+            <li><a href="#">company table</a></li>
+            <li class="selected"><a href="#">company page</a></li>
           </ul>
-          <div style="border-radius: 5px">
-            <ul class="side-menu">
-              <li><a href="#">company data</a></li>
-              <li><a href="#">company table</a></li>
-              <li class="selected"><a href="#">company page</a></li>
-            </ul>
-          </div>
         </div>
-        <div class="main">
-
+      </div>
+      <div class="main float-left">
+        <img class="float-left company-logo" style="padding: 15px; background-color: white; border-radius: 5px; box-shadow: 1px 1px 3px #bcbcbc;" src="../images/squares.png" />
+        <div style="margin-left: 15px; display: inline-block;">
+          <h1>Microsoft</h1>
+          <p class="company-location">Redmond, WA USA</p>
+        </div>
+        <p>
+          At Microsoft, our mission is to empower every person and every organization on the planet to achieve more. Our mission is grounded in both the world in which we live and the future we strive to create. <a href="#">View More...</a>
+        </p>
+          <!--        <p>-->
+<!--          At Microsoft, our mission is to empower every person and every organization on the planet to achieve more. Our mission is grounded in both the world in which we live and the future we strive to create. Today, we live in a mobile-first, cloud-first world, and the transformation we are driving across our businesses is designed to enable Microsoft and our customers to thrive in this world.-->
+<!--        </p>-->
+        <div class="activity">
+          <ul>Activities
+            <li>
+              <p>Cloud Orchestration replaced Microsoft on 1 property</p>
+            </li>
+          </ul>
+        </div>
+        <div class="similar-companies">
+          <ul>Activities
+            <li>
+              <p>Cloud Orchestration replaced Microsoft on 1 property</p>
+            </li>
+          </ul>
+        </div>
+        <div class="spend-history">
+          <ul>Activities
+            <li>
+              <p>Cloud Orchestration replaced Microsoft on 1 property</p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-    <div class="footer">
+    <div class="footer wrapper">
       <ul class="bottom-menu left">
         <li><a href="#">Pricing</a></li>
         <li><a href="#">Blog</a></li>
